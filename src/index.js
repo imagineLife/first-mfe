1d6f6edf2a4a05c858c08c21fc7de750be9f64ca
Eric Laursen <ericjlaursen@gmail.com>
2022-12-12 07:58:29 -0500

    buncha components, new dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './app'
+
 const MFE_PARENT_DIV = 'mfe-root';
-function Header() {
-  return <h1>Init Header Here</h1>;
-}
-
-
 const rootElement = document.getElementById(MFE_PARENT_DIV);
+
 if (rootElement) {
   const root = createRoot(rootElement);
-root.render(<Header />)
+root.render(<App />)
 }
---
+++ src/layouts/default/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import NavBar from './../../components/nav';
+
+export default function DefaultLayout({ window, children }) {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <NavBar window={window} />
+      <Box component="main" sx={{ p: 3 }}>
+        {/* "spacer" toolabr to "push down" content top to below the "real" toolbar in the NavBar */}
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+}
