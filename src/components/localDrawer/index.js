92eea02ba4587bd14a49c32799673e0c92377223
Eric Laursen <ericjlaursen@gmail.com>
2022-12-12 10:08:46 -0500

    brief write-up #1

---
+++ src/components/localDrawer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function LocalDrawer({ onClick, navItems }) {
+  return (
+    <Box onClick={onClick} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        AppLocal
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+}
--- src/components/reader/index.js
+++ src/components/reader/index.js
@@ -1,35 +1,105 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+function Section({ sx, h1, h2, h3, h4, h5, h6, children }) { 
+  return (
+    <Fragment>
+      {h1 && <Typography variant="h1" sx={{...sx}}>{h1}</Typography>}
+      {h2 && <Typography variant="h2" sx={{...sx}}>{h2}</Typography>}
+      {h3 && <Typography variant="h3" sx={{...sx}}>{h3}</Typography>}
+      {h4 && <Typography variant="h4" sx={{...sx}}>{h4}</Typography>}
+      {h5 && <Typography variant="h5" sx={{...sx}}>{h5}</Typography>}
+      {h6 && <Typography variant="h6" sx={{...sx}}>{h6}</Typography>}
+      {children}
+    </Fragment>
+  );
+}
+
+function GettingStarted() {
+  return (
+    <Section h2="Initial Setup">
+      <Section h3="Dependencies" sx={{ p: 3 }}>
+        <Typography variant="body1">
+          The app is setup without the modulke federation, a "vanilla" bare-bones react-based
+          frontend project.
+        </Typography>
+        <Typography variant="body1">
+          The dependencies are relatively simple: react, webpack, babel, and material ui{' '}
+          <i>(and all of their dependencies)</i>:
+        </Typography>
+        <code style={{ whiteSpace: 'pre' }}>
+          {`"dependencies": {
+    "@babel/core": "^7.20.5",
+    "@babel/preset-env": "^7.20.2",
+    "@babel/preset-react": "^7.18.6",
+    "@emotion/react": "^11.10.5",
+    "@emotion/styled": "^11.10.5",
+    "@mui/icons-material": "^5.10.16",
+    "@mui/material": "^5.10.17",
+    "babel-loader": "^9.1.0",
+    "html-webpack-plugin": "^5.5.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "webpack": "^5.75.0",
+    "webpack-cli": "^5.0.1"
+  }`}
+        </code>
+      </Section>
+
+      <Section h3="UI Parts" sx={{ p: 3 }}>
+        <Typography variant="body1">
+          <b>THE APP</b> starts with a layout and this text content.
+        </Typography>
+        <Typography variant="body1">
+          <b>THE LAYOUT</b> consists of a wrapper around a nav and some content.
+        </Typography>
+        <Typography variant="body1">
+          <b>THE NAV</b> is the most complex part. There are a bunch of more components in the nav
+          than listed here, but the ux outcome of the responsive nav is
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="An AppBar"
+              secondary="The Sandwich Icon in mobile mode and the toolbar showing 3 menu items (made up items here)"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="A Drawer" secondary={'The "open" drawer in mobile mode'} />
+          </ListItem>
+        </List>
+        <Typography variant="body1">
+          <b>THE TEXT</b> content is served as part of a <code>{`<Reader />`}</code> component,
+          served as a child of the <code>{`<DefaultLayout />`}</code> component
+        </Typography>
+        <Typography variant="h4">
+          <Typography variant="body1">
+            The <code>{`<App />`}</code> puts the layout and the Reader together in a small file
+            that reads <i>(note the "window" gets passed around for a detail)</i>
+          </Typography>
+          <Typography variant="body1">
+            <code style={{ whiteSpace: 'pre' }}>{`export default function App({window}) {
+  return (
+    <DefaultLayout window={window}>
+      <Reader />
+    </DefaultLayout>
+  );
+}`}</code>
+          </Typography>
+        </Typography>
+      </Section>
+    </Section>
+  );
+}
 
 export default function Reader() {
   return (
-    <Typography>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde fugit veniam eius,
-      perspiciatis sunt? Corporis qui ducimus quibusdam, aliquam dolore excepturi quae. Distinctio
-      enim at eligendi perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod itaque
-      exercitationem, at ab sequi qui modi delectus quia corrupti alias distinctio nostrum. Minima
-      ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed numquam quibusdam at
-      officia sapiente porro maxime corrupti perspiciatis asperiores, exercitationem eius nostrum
-      consequuntur iure aliquam itaque, assumenda et! Quibusdam temporibus beatae doloremque
-      voluptatum doloribus soluta accusamus porro reprehenderit eos inventore facere, fugit,
-      molestiae ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque soluta,
-      aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem! Cumque, eligendi unde
-      aliquid minus quis sit debitis obcaecati error, delectus quo eius exercitationem tempore.
-      Delectus sapiente, provident corporis dolorum quibusdam aut beatae repellendus est labore
-      quisquam praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa deleniti
-      modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non fugiat praesentium doloremque
-      architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore molestiae dicta blanditiis
-      est expedita eius debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
-      praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat voluptate iure labore,
-      repellendus beatae quia unde est aliquid dolor molestias libero. Reiciendis similique
-      exercitationem consequatur, nobis placeat illo laudantium! Enim perferendis nulla soluta magni
-      error, provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
-      suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto. Illum, corrupti? Fugiat
-      quidem accusantium nulla. Aliquid inventore commodi reprehenderit rerum reiciendis! Quidem
-      alias repudiandae eaque eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
-      ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
-      Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem dolores sunt
-      inventore perferendis, aut sapiente modi nesciunt.
-    </Typography>
+    <Fragment>
+      <Typography variant="h1">Getting Started</Typography>
+      <GettingStarted />
+    </Fragment>
   );
 }
