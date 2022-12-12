32c814b17836223d726cf601f1224c509b3f0e3c
Eric <ericjlaursen@gmail.com>
2022-12-12 10:29:51 -0500

    brief write-up #1 (#3)

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -8,7 +8,6 @@ import React, { Fragment, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
@@ -18,33 +17,18 @@ import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+const LocalDrawer = React.lazy(() => import('./../localDrawer'));
 
 const drawerWidth = 240;
 const navItems = ['Home', 'About', 'Contact'];
+
+
 export default function Nav({window}) {
   const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   const container = window !== undefined ? () => window().document.body : undefined;
   return (
@@ -90,7 +74,11 @@ export default function Nav({window}) {
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
           }}
         >
-          {drawer}
+          {mobileOpen && (
+            <React.Suspense fallback={<span></span>}>
+              <LocalDrawer onClick={handleDrawerToggle} navItems={navItems} />
+            </React.Suspense>
+          )}
         </Drawer>
       </Box>
     </Fragment>
