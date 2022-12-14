d5a63001e0076114e3470a111b646bed68081806
Eric Laursen <ericjlaursen@gmail.com>
2022-12-13 19:01:32 -0500

    lazy-load parts

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
-import DefaultLayout from './../layouts/default';
-import Reader from './../components/reader'
+const DefaultLayout = React.lazy(() => import('./../layouts/default'));
+const Reader = React.lazy(() => import('./../components/reader'));
 
 /*
   The app is a composition of a DefaultLayout and a Reader
   - Default layout contains the responsive toolbar + drawer
   - reader is a buncha text
 */
-export default function App({window}) {
+export default function App({ window }) {
   return (
-    <DefaultLayout window={window}>
-      <Reader />
-    </DefaultLayout>
+    <React.Suspense fallback={<span></span>}>
+      <DefaultLayout window={window}>
+        <Reader />
+      </DefaultLayout>
+    </React.Suspense>
   );
 }
