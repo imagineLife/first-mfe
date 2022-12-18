08412e4c047183971d4b76fe12f332de3dee6c60
Eric Laursen <ericjlaursen@gmail.com>
2022-12-18 08:36:20 -0500

    react lazy load the remote

--- host/src/layouts/default/index.js
+++ host/src/layouts/default/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import { Nav as NavBar } from 'nav/Nav';
+const NavBar = React.lazy(() => import('nav/Nav'));
 
 export default function DefaultLayout({ window, children }) {
   return (
     <Box sx={{ display: 'flex' }}>
-      <NavBar window={window} />
+      <React.Suspense fallback={<span />}>
+        <NavBar window={window} />
+      </React.Suspense>
       <Box component="main" sx={{ p: 3 }}>
         {/* "spacer" toolabr to "push down" content top to below the "real" toolbar in the NavBar */}
         <Toolbar />
