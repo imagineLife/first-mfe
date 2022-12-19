07a55a56fb514d13b2240847babc3b78f9314744
Eric Laursen <ericjlaursen@gmail.com>
2022-12-18 21:02:35 -0500

    init

---
+++ host/src/components/reader/bundlingForProd.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Section from './section';
+
+import { Typography } from '@mui/material';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemText from '@mui/material/ListItemText';
+
+export default function BundlingForPod() {
+  return (
+    <Section h3="Bundling For Production" sx={{ p: 3 }}>
+      <Typography variant="body1">Init</Typography>
+    </Section>
+  );
+}
