c4cad94efc264cd2fd8e381ac8d8b470b7d30d43
Eric Laursen <ericjlaursen@gmail.com>
2022-12-19 07:16:20 -0500

    host app

--- host/src/components/reader/bundlingForProd.js
+++ host/src/components/reader/bundlingForProd.js
@@ -9,7 +9,22 @@ import { Typography } from '@mui/material';
 export default function BundlingForPod() {
   return (
     <Section h3="Bundling For Production" sx={{ p: 3 }}>
-      <Typography variant="body1">Init</Typography>
+      <Typography variant="body1">
+        Traditional Single-Page-Applications built with react and Webpack are a bit less to deal
+        with than this type of federated module setup. This setups creates 2 sets of "bundled" code,
+        one for the host and one for the remote, rather than one bundle for the single app.
+      </Typography>
+
+      <Section h4="Bundling The Host" sx={{ p: 3 }}>
+        <Typography variant="body1">
+          The host gets bundled like a traditional single-page-application. Running something like{' '}
+          <code>{`webpack --mode=production`}</code>, maybe assigned to an npm command (
+          <i>
+            <code>{`npm run build`}</code>
+          </i>
+          ) will convert the code to production-ready output into a "dist" directory.
+        </Typography>
+      </Section>
     </Section>
   );
 }
